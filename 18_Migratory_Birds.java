0a6ed82f9ecb360baf7699a7ec8d343111416405
a-tan <>
2018-05-30 03:18:39 +0900

    追加

---
+++ 18_Migratory_Birds.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the migratoryBirds function below.
+    static int migratoryBirds(int[] ar) {
+        int[] count = { 0, 0, 0, 0, 0 };
+        for (int i = 0; i < ar.length; i++) {
+            count[ar[i] - 1] += 1;
+        }
+        int result = 0;
+        int maxNum = 0;
+        for (int i = 0; i < count.length; i++) {
+            if (maxNum < count[i]) {
+                maxNum = count[i];
+                result = i + 1;
+            }
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = migratoryBirds(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
